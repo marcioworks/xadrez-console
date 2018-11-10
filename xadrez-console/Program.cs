2893fd64f103042b54c5c20788483f3435c82d51
marcioworks <marcioadsworks@gmail.com>
2018-11-10 15:27:38 -0300

    classe tabuleiro iniciada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using tabuleiro;
 
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
+            Posicao p;
+
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("posição: " + p);
+
+            Console.ReadLine();
+
+
         }
     }
 }
---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace tabuleiro {
+    class Posicao {
+        int linha { get; set; }
+        int coluna { get; set; }
+
+        public Posicao(int linha, int coluna) {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public override string ToString() {
+            return linha
+                + ", "
+                + coluna;
+        }
+    }
+}
